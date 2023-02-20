cd5d45643dc4824edc02a8d2a3d8d3c516027544
Tornik2 <tornik2@yahoo.com>
2023-02-20 21:23:32 +0400

    added cursor-pointers

--- data.js
+++ data.js
@@ -1,23 +1,26 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+  
+  
 export const menuArray = [
     {
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
+        id: uuidv4(),
         price: 14,
         emoji: "🍕"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
+        price: 11,
         emoji: "🍔",
-        id: 1
+        id: uuidv4()
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: uuidv4()
     }
 ]
--- index.js
+++ index.js
@@ -1,45 +1,123 @@
 import {menuArray} from "/data.js"
-const orderArray = []
+
+
+const modal = document.getElementById('modal')
+const submitBtn = document.getElementById('submit-button')
+const completeBtn = document.getElementById('complete')
+const closeModalBtn = document.getElementById('close-modal')
+
+const orderSection = document.getElementById('order-section')
+const orderConfirmed = document.getElementById('order-confirmed')
+
+const inputName = document.getElementById('input-name')
+const inputNumber = document.getElementById('input-number')
+const inputCvv = document.getElementById('input-cvv')
+
+let total = 0
+let orderArray = []
+const totalPrice = document.getElementById("total-price")
+
+
+
+
 document.addEventListener('click', function(e){
     if (e.target.dataset.button) {
         handleAddButton(e.target.dataset.button)
-    }
+        orderSection.style.display = 'block'
+        orderConfirmed.style.display = 'none'
+    } else if (e.target.dataset.remove) {
+        handleRemoveButton(e.target.dataset.remove)
+    } 
     
+    
+})
+
+submitBtn.addEventListener('click', function(e){
+   e.preventDefault()
+   
+   if(inputName.value && inputNumber.value && inputCvv.value){
+       orderSection.style.display = 'none'
+       modal.style.display = 'none'
+       orderConfirmed.style.display = 'block'
+       
+       inputCvv.value = ''
+       inputName.value = ''
+       inputNumber.value = ''
+       orderArray = []
+       total = 0
+   }
+       
+})
+
+
+
+completeBtn.addEventListener('click', function(){
+  modal.style.display = 'block'  
+})
+
+closeModalBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
 })
 
 function handleAddButton(itemId) {
-    
+   
     const itemToAdd = menuArray.filter(function (item){ 
         return item.id.toString() === itemId
-})
+})[0]
+
 orderArray.push(itemToAdd)
-console.log(orderArray)
+
+total += itemToAdd.price
+
+totalPrice.innerHTML = "$" + total
+
 render()
+
 } 
 
+function handleRemoveButton(itemId) {
+    const itemToRemove = menuArray.filter(function(item) {
+        return itemId === item.id.toString()
+    })[0]
+    
+    const index = orderArray.indexOf(itemToRemove)
+    orderArray.splice(index, 1)
+    
+    total -= itemToRemove.price
+    
+    totalPrice.innerHTML = "$" +  total
+    
+    render()
+    
+    if (total === 0) {
+        orderSection.style.display = 'none'
+    }
+    
+}
 
-function getitSection() {
+function getOrderSection() {
     
     let orderSection = ''
     
-    orderArray.forEach(function(items){
-    items.forEach(function(item){
+    orderArray.forEach(function(item){
+    
         
         orderSection += `
                          <div class="order-list-div">
                                 <h3 class="order-list">${item.name}</h3>
-                                <button class="remove-button">remove</button>
-                                <h3 class="item-price" id="item-price">${item.price}<h3>
+                                <button class="remove-button" data-remove="${item.id}" >remove</button>
+                                <h3 class="item-price" id="item-price">$${item.price}<h3>
                     </div>`
 
     })
-    return orderSection
-    })
+    
+    
+    
     return orderSection
     
     
     }
-console.log(getitSection())
+
 
 
 function getitforHtml() {
@@ -55,7 +133,7 @@ menuArray.forEach(function(item){
                 <div class="description">
                     <p class="name">${item.name}</p>
                     <p class="ingredient">${item.ingredients}</p>
-                    <p class="price">${item.price}</p>
+                    <p class="price">$${item.price}</p>
                 </div>
                         <div class="button-div">
                         <button class="add-button" data-button="${item.id}">+</button>
@@ -67,27 +145,26 @@ menuArray.forEach(function(item){
 return menu
 }
 
-function render() {
+function getThanks() {
+    let thanksText = ''
+    
+     thanksText = `
+            <div class="thanks" id="thanks">
+                <h5> Thanks, ${inputName.value}! Your order is on its way!</h5>
+            </div>
+     `
      
+     return thanksText
+}
+
+function render() {
+    
     document.querySelector('main').innerHTML = getitforHtml()
-    document.getElementById('to-render').innerHTML = getitSection()
+    document.getElementById('to-render').innerHTML = getOrderSection()
+    orderConfirmed.innerHTML = getThanks()
+    
     
 }
 
 render()
 
-/*`
-                        <h3 class="your-order"> Your order </h3>
-                    <div class="order-list-div">
-                                <h3 class="order-list">ITEMS</h3>
-                                <button class="remove-button">remove</button>
-                                <h3 class="item-price" id="item-price">${item.price}<h3>
-                    </div>
-                            <div class="total-price-div">
-                        <h3 class="total"> Total price </h3>
-                        <h3 class="total-price">20</h3>
-                            </div>
-                            <div class="complete-div">
-                                <button class="complete">Complete order</button>
-                     </div>`
-*/
